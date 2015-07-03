4cf76ddc6eb2442fd95adbf2103861fbcbdf8d6c
tankerkiller125 <tankerkiller125@gmail.com>
2015-07-02 20:49:28 -0400

    Using Composer Autoloader instead of manually adding files.

--- lib/Castle.php
+++ lib/Castle.php
@@ -15,17 +15,4 @@ if (!function_exists('lcfirst'))
   }
 }
 
-require(dirname(__FILE__) . '/Castle/Castle.php');
-require(dirname(__FILE__) . '/Castle/CookieStore.php');
-require(dirname(__FILE__) . '/Castle/Errors.php');
-require(dirname(__FILE__) . '/Castle/SessionToken.php');
-require(dirname(__FILE__) . '/Castle/TokenStore.php');
-require(dirname(__FILE__) . '/RestModel/Resource.php');
-require(dirname(__FILE__) . '/RestModel/Model.php');
-require(dirname(__FILE__) . '/Castle/Models/Account.php');
-require(dirname(__FILE__) . '/Castle/Models/Context.php');
-require(dirname(__FILE__) . '/Castle/Models/Event.php');
-require(dirname(__FILE__) . '/Castle/Models/User.php');
-require(dirname(__FILE__) . '/Castle/JWT.php');
-require(dirname(__FILE__) . '/Castle/CurlTransport.php');
-require(dirname(__FILE__) . '/Castle/Request.php');
+require(dirname(dirname(__FILE__)) . '/vendor/autoload.php');
