7346cc558757c4f392f150734d1a99571709dbd0
Dave Blakey <dave@blakey.co>
2019-07-25 09:14:47 +0200

    Return hints for compatability with PHPUnit changes

--- test/CastleTest.php
+++ test/CastleTest.php
@@ -8,7 +8,7 @@ class CastleTest extends Castle_TestCase
     Castle::setApiKey('secretkey');
   }
 
-  public function setUp()
+  public function setUp(): void
   {
     $_SESSION = array();
     $_COOKIE = array();
