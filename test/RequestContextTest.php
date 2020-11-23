55fa26baed8dc2c3813b04b544aaab84cd44c433
Maria Korlotian <maria.korlotian@castle.io>
2020-11-23 15:38:01 +0100

    Release 2.1.0 (#36)

--- test/RequestContextTest.php
+++ test/RequestContextTest.php
@@ -30,7 +30,7 @@ class CastleRequestContextTest extends \Castle_TestCase
   }
 
   public function contextJsonProvider() {
-    return array(array('{"client_id":"1ccf8dee-904b-4d20-8a88-55ded468bcc5","ip":"8.8.8.8","headers":{"User-Agent":"TestAgent","X-Castle-Client-Id":"1ccf8dee-904b-4d20-8a88-55ded468bcc5"},"user_agent":"TestAgent","library":{"name":"castle-php","version":"2.0.0"}}'));
+    return array(array('{"client_id":"1ccf8dee-904b-4d20-8a88-55ded468bcc5","ip":"8.8.8.8","headers":{"User-Agent":"TestAgent","X-Castle-Client-Id":"1ccf8dee-904b-4d20-8a88-55ded468bcc5"},"user_agent":"TestAgent","library":{"name":"castle-php","version":"2.1.0"}}'));
   }
 
   /**
