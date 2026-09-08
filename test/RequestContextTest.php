a08ab1290f13dac6461a86376bcfb3ee015be549
Bartosz <bartesrlz@gmail.com>
2026-09-08 14:56:09 +0200

    Read the request-context version from Castle::VERSION in specs.

--- test/RequestContextTest.php
+++ test/RequestContextTest.php
@@ -28,7 +28,8 @@ class CastleRequestContextTest extends \Castle_TestCase
   }
 
   public function contextJsonProvider() {
-    return array(array('{"ip":"8.8.8.8","headers":{"User-Agent":"TestAgent","X-Castle-Client-Id":"1ccf8dee-904b-4d20-8a88-55ded468bcc5"},"library":{"name":"castle-php","version":"4.1.0"}}'));
+    $contexts = $this->contextProvider();
+    return array(array(json_encode($contexts[0][0])));
   }
 
   /**
